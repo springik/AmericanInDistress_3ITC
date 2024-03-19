e4e51c86fa2b2684f61770fa8b490d8fc0b725eb
IndustrialBro <buro.krystof@seznam.cz>
2024-03-19 14:01:41 +0100

    lkjůlkjůlkj

---
+++ Assets/Scripts/BuildingSystem/BuildingManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class BuildingManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> buildings = new List<GameObject>();
+    
+
+    
+}
