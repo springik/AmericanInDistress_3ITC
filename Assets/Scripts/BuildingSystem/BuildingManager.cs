f7cde88387fe126010bb2fff0a38b6a1aebc35d5
springik <springik.games@email.cz>
2024-03-19 14:15:38 +0100

    Update BM.cs

--- Assets/Scripts/BuildingSystem/BuildingManager.cs
+++ Assets/Scripts/BuildingSystem/BuildingManager.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public sealed class BuildingManager : MonoBehaviour
 {
-    [SerializeField]
-    List<Building> buildings = new List<Building>();
-    
+    public static BuildingManager instance;
+    public static BuildingManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    Building chosenBuilding = null;
 
-    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField]
+    internal List<Building> buildings { get; private set; } = new List<Building>();
+
+    private void Start()
+    {
+        Debug.Log(Instance);
+    }
+    void ChooseBuilding(Building building)
+    {
+        chosenBuilding = building;
+    }
 }
