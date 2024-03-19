81609eef3b3541107e236060af36fe9aaf11dd21
IndustrialBro <buro.krystof@seznam.cz>
2024-03-19 13:45:53 +0100

    Test building prefab + new folders

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,22 +5,24 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [SerializeField]
+    GameObject building;
     public TileData TileData { get; set; }
 
     internal void SetBuilding(GameObject building)
     {
         building.transform.SetParent(transform);
-        building.transform.localPosition = Vector3.zero;
+        building.transform.localPosition = new Vector3(1.25f, 0, 1.25f);
     }
 
     private void OnMouseDown()
     {
-        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-        Debug.Log(cube);
+        //Debug.Log(cube);
 
         var cmd = new BuildCommand(
-            cube,
+            building,
             this
             );
 
